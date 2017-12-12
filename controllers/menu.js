dea7869202e2fefb4afc5538a6c3abfcc08c791e
yayo <yangyong@navercorp.com>
2017-12-12 15:52:20 +0800

    modify menu index response

--- controllers/menu.js
+++ controllers/menu.js
@@ -5,22 +5,24 @@
 
 const controller = {
   index (req, res) {
-    res.send([{
-      name: '扬州炒饭',
-      pic: 'food_default.jpg',
-      taste: ['salty'],
-      material: [
-        { name: '鸡蛋', quantity: '2个' },
-        { name: '米饭', quantity: '300g' },
-        { name: '火腿肠', quantity: '100g' },
-      ],
-      category: { name: '小吃', tab: 'snack' },
-      cost: {
-        time: 10,
-        money: 5
-      },
-      times: 21
-    }]);
+    res.json({
+      snack: [{
+        name: '扬州炒饭',
+        pic: 'food_default.jpg',
+        taste: ['salty'],
+        material: [
+          { name: '鸡蛋', quantity: '2个' },
+          { name: '米饭', quantity: '300g' },
+          { name: '火腿肠', quantity: '100g' },
+        ],
+        category: { name: '小吃', tab: 'snack' },
+        cost: {
+          time: 10,
+          money: 5
+        },
+        times: 21
+      }]
+    });
   }
 };
 
